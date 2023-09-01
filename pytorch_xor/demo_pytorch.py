897835caa99ce845725caa261386fed88a1bdb76
azzy13 <azharhasan013@gmail.com>
2023-09-01 16:51:48 -0500

    Fixes

--- pytorch_xor/demo_pytorch.py
+++ pytorch_xor/demo_pytorch.py
@@ -1,7 +1,6 @@
-# import libraries
 import torch
 import torch.nn as nn
-from torch.autograd import Variable
+import torch.optim as optim
 import numpy as np
 
 import rclpy
@@ -9,78 +8,93 @@ from rclpy.node import Node
 
 from std_msgs.msg import String
 
-# create data
-Xs = torch.Tensor([[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]])
-y = torch.Tensor([0.0, 1.0, 1.0, 0.0]).reshape(Xs.shape[0], 1)
 
-
-class XOR(nn.Module):
+class MinimalPublisher(Node):
     def __init__(self):
-        super(XOR, self).__init__()
-        self.linear = nn.Linear(2, 2)
-        self.Sigmoid = nn.Sigmoid()
-        self.linear2 = nn.Linear(2, 1)
+        super().__init__("minimal_publisher")
+        self.publisher_ = self.create_publisher(String, "topic", 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
 
-    def forward(self, input):
-        x = self.linear(input)
-        sig = self.Sigmoid(x)
-        yh = self.linear2(sig)
-        return yh
+    def timer_callback(self):
+        msg = String()
+        msg.data = self.string_data
+        # Pass the string data from train_xor_network to timer_callback
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
 
 
-xor_network = XOR()
+def train_xor_network():
+    Xs = torch.Tensor([[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0]])
+    y = torch.Tensor([0.0, 1.0, 1.0, 0.0]).reshape(Xs.shape[0], 1)
 
-epochs = 1000
-mseloss = nn.MSELoss()
-optimizer = torch.optim.Adam(xor_network.parameters(), lr=0.03)
-all_losses = []
-current_loss = 0
-plot_every = 50
+    # Define the XOR neural network
+    xor_network = nn.Sequential(nn.Linear(2, 2), nn.Sigmoid(), nn.Linear(2, 1))
 
-for epoch in range(epochs):
-    # input training example and return the prediction
-    yhat = xor_network.forward(Xs)
-    # calculate MSE loss
-    loss = mseloss(yhat, y)
-    # backpropogate through the loss gradiants
-    loss.backward()
-    # update model weights
-    optimizer.step()
-    # remove current gradients for next iteration
-    optimizer.zero_grad()
-    # append to loss
-    current_loss += loss
-    if epoch % plot_every == 0:
-        all_losses.append(current_loss / plot_every)
-        current_loss = 0
-    # print progress
-    if epoch % 500 == 0:
-        print(f"Epoch: {epoch} completed")
+    epochs = 1000
+    mseloss = nn.MSELoss()
+    optimizer = torch.optim.Adam(xor_network.parameters(), lr=0.03)
+    all_losses = []
+    current_loss = 0
+    plot_every = 50
 
-# show weights and bias
-for name, param in xor_network.named_parameters():
-    if param.requires_grad:
-        print(name, param.data)
+    for epoch in range(epochs):
+        # Input training example and return the prediction
+        yhat = xor_network(Xs)
+        # Calculate MSE loss
+        loss = mseloss(yhat, y)
+        # Backpropagate through the loss gradients
+        loss.backward()
+        # Update model weights
+        optimizer.step()
+        # Remove current gradients for the next iteration
+        optimizer.zero_grad()
+        # Append to the loss
+        current_loss += loss
+        if epoch % plot_every == 0:
+            all_losses.append(current_loss / plot_every)
+            current_loss = 0
+        # Print progress
+        if epoch % 500 == 0:
+            print(f"Epoch: {epoch} completed")
 
-# test input
-input = torch.tensor([0.0, 1.0])
-print("For inputs", input)
-out = xor_network(input)
-# convert to string
-output = out.detach().numpy()
-string = np.array2string(output, precision=0)
-print(string)
+    # Show weights and bias
+    for name, param in xor_network.named_parameters():
+        if param.requires_grad:
+            print(name, param.data)
+
+    # Test input
+    input = torch.tensor([0.0, 1.0])
+    print("For inputs", input)
+    out = xor_network(input)
+    # Convert to string
+    output = out.detach().numpy()
+    string = np.array2string(output, precision=0)
+    print(string)
+
+    # Return the string data to be passed to timer_callback
+    return string
 
 
 def main(args=None):
-    # rclpy.init(args=args)
+    rclpy.init(args=args)
+    minimal_publisher = MinimalPublisher()
 
-    xor = XOR()
+    # Call the function to train the XOR network and get the string data
+    string_data = train_xor_network()
 
-    # rclpy.spin(XOR)
+    # Pass the string data to the timer_callback
+    minimal_publisher.string_data = string_data
 
-    # XOR.destroy_node()
-    # rclpy.shutdown()
+    rclpy.spin(minimal_publisher)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_publisher.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == "__main__":
